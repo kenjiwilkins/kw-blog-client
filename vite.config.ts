9c2fe323dd443dec25babe1afe3889b5dc306d93
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2023-03-22 08:09:14 +0900

    use hash history

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { fileURLToPath, URL } from "node:url";
 /** @type {import('vite').UserConfig} */
 const viteConfig = defineViteConfig({
   plugins: [vue()],
-  base: "/<REPO>/",
+  base: "/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
