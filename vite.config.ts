63f07634fe424babcb652bdd45d2ca08b81da8ee
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2023-06-10 17:15:55 +0900

    add sentry configs (#17)
    * add sentry configs
    
    * fix error
    
    * fix import meta

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,40 @@
 /// <reference types="vitest" />
-import { defineConfig as defineViteConfig, mergeConfig } from "vite";
-import { defineConfig } from "vitest/config";
+import { defineConfig as defineViteConfig, loadEnv } from "vite";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
+import { version } from "./package.json";
 
 // https://vitejs.dev/config/
 /** @type {import('vite').UserConfig} */
-const viteConfig = defineViteConfig({
-  plugins: [vue()],
-  base: "/",
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default ({ mode }: any) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  console.log("TOKEN", process.env.VITE_APP_SENTRY_TOKEN);
+  return defineViteConfig({
+    build: {
+      sourcemap: true,
     },
-  },
-});
-
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      coverage: {
-        provider: "istanbul",
-        include: ["src/**/*.{js,ts,vue}"],
+    define: {
+      "process.env.RELEASE_VERSION": `"kw-blog-client@${version}"`,
+    },
+    plugins: [
+      vue(),
+      sentryVitePlugin({
+        org: "kenji-wilkins",
+        project: "kw-blog-client",
+        // @ts-ignore
+        token: process.env.VITE_APP_SENTRY_TOKEN,
+        release: {
+          name: `kw-blog-client@${version}`,
+        },
+      }),
+    ],
+    base: "/",
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     },
-  })
-);
+  });
+};
