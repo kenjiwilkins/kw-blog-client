92aae52394c8be3bd7a232c7cb6711d7e78682fe
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2023-06-10 17:18:00 +0900

    Release v1.2.26 (#18)
    * Bump vite from 3.1.3 to 3.2.7 (#12)
    
    Bumps [vite](https://github.com/vitejs/vite/tree/HEAD/packages/vite) from 3.1.3 to 3.2.7.
    - [Release notes](https://github.com/vitejs/vite/releases)
    - [Changelog](https://github.com/vitejs/vite/blob/v3.2.7/packages/vite/CHANGELOG.md)
    - [Commits](https://github.com/vitejs/vite/commits/v3.2.7/packages/vite)
    
    ---
    updated-dependencies:
    - dependency-name: vite
      dependency-type: direct:development
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * add sentry configs (#17)
    
    * add sentry configs
    
    * fix error
    
    * fix import meta
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
 import { createApp } from "vue";
+import * as sentry from "@sentry/vue";
+import { CaptureConsole } from "@sentry/integrations";
 import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
 
 const app = createApp(App);
+sentry.init({
+  app,
+  dsn: import.meta.env.VITE_APP_SENTRY_DSN,
+  release: import.meta.env.RELEASE_VERSION,
+  integrations: [
+    new sentry.BrowserTracing({
+      routingInstrumentation: sentry.vueRouterInstrumentation(router),
+    }),
+    new sentry.Replay(),
+    new CaptureConsole({
+      levels: ["error", "warn", "log"],
+    }),
+  ],
+  tracesSampleRate: 0.1,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 app.use(router);
 
