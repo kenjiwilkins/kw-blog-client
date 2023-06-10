d34935d90d3cd0be864ffeb0959f98411aa4821f
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2023-06-10 10:20:20 +0900

    add rate-limit, logger

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
 import * as express from "express";
+import rateLimit from "express-rate-limit";
+import logger from "morgan";
 
 const app = express();
 app.use(express.static(__dirname + "/dist"));
+app.use(
+  rateLimit({
+    windowMs: 10000, // 1 minute
+    max: 5, // limit each IP to 5 requests per windowMs
+    message: "Too many requests, please try again later.",
+  })
+);
+app.use(logger("common"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
