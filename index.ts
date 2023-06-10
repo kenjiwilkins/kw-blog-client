06b2798545425a1352f91ae5a958502822b153b4
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2023-06-10 10:56:29 +0900

    release v1.2.23 (#14)
    * Bump @sideway/formula from 3.0.0 to 3.0.1
    
    Bumps [@sideway/formula](https://github.com/sideway/formula) from 3.0.0 to 3.0.1.
    - [Release notes](https://github.com/sideway/formula/releases)
    - [Commits](https://github.com/sideway/formula/compare/v3.0.0...v3.0.1)
    
    ---
    updated-dependencies:
    - dependency-name: "@sideway/formula"
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Update version
    
    * add express (#13)
    
    * add express
    
    * fix by lint
    
    * add rate-limit, logger
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ index.ts
@@ -0,0 +1,27 @@
+import * as express from "express";
+import rateLimit from "express-rate-limit";
+import logger from "morgan";
+
+const app = express();
+app.use(express.static(__dirname + "/dist"));
+app.use(
+  rateLimit({
+    windowMs: 10000, // 1 minute
+    max: 5, // limit each IP to 5 requests per windowMs
+    message: "Too many requests, please try again later.",
+  })
+);
+app.use(logger("common"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// return index.html to all routes
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/dist/index.html");
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory("/"),
   routes: [
     {
       path: "/",
