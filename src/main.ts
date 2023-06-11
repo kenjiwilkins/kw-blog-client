4abfe631fa90608567840be20ff929b1d688ad2f
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2023-06-11 14:51:35 +0900

    update sentry settings

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,10 @@ sentry.init({
   release: import.meta.env.RELEASE_VERSION,
   integrations: [
     new sentry.BrowserTracing({
+      tracePropagationTargets: [
+        "localhost",
+        /^https:\/\/cdn.contentful.com\/spaces\/2cmxpw7zfb54\/environments\/master\//,
+      ],
       routingInstrumentation: sentry.vueRouterInstrumentation(router),
     }),
     new sentry.Replay(),
@@ -23,6 +27,21 @@ sentry.init({
   tracesSampleRate: 0.1,
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
+});
+
+app.config.errorHandler = (err) => {
+  sentry.captureException(err);
+  console.log("ERROR", err);
+};
+
+window.addEventListener("error", (event) => {
+  sentry.captureException(event);
+  console.log("ERROR", event);
+});
+
+window.addEventListener("unhandledrejection", (event) => {
+  sentry.captureException(event);
+  console.log("EXCEPTION", event);
 });
 
 app.use(router);
