7f2ac31a7bee9baf701b04a85e1e151351833478
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2023-06-14 16:20:40 +0900

    add: navigate 404 no article

--- src/views/ArticleEndView.vue
+++ src/views/ArticleEndView.vue
@@ -8,18 +8,22 @@ import ContentWrapper from "@/components/ContentWrapper.vue";
 import ArticleBody from "@/components/ArticleBody.vue";
 import { getArticle } from "@/api";
 import { ref, onMounted } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { setTitle } from "@/utils";
 const article = ref();
 const route = useRoute();
+const router = useRouter();
 onMounted(() => {
-  getArticle(typeof route.params.id === "string" ? route.params.id : "").then(
-    (entry) => {
+  getArticle(typeof route.params.id === "string" ? route.params.id : "")
+    .then((entry) => {
       article.value = entry;
       typeof article.value.fields.title === "string"
         ? setTitle(article.value.fields.title)
         : console.error("Article title not found");
-    }
-  );
+    })
+    .catch((error) => {
+      console.error(error);
+      router.push("/404");
+    });
 });
 </script>
