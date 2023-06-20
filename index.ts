7dd9712dcd3f8961dae07e07e7b1ed87705094a7
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2023-06-20 23:44:18 +0900

    add: twitter OGP

--- index.ts
+++ index.ts
@@ -27,30 +27,71 @@ app.use(
 app.use(logger("common"));
 
 app.get("/article/:articleId", async (req, res) => {
-  const articleId = req.params.articleId;
-  const article = (await getArticle(articleId)) as any;
-  const fileUrl = article.fields.thumbnail.fields.file.url;
-  fs.readFile(__dirname + "/dist/index.html", "utf8", (err, htmlData) => {
-    if (err) {
-      console.error("Error during file reading", err);
+  try {
+    const articleId = req.params.articleId;
+    const article = (await getArticle(articleId)) as any;
+    console.log(article.fields.tags);
+    const articleFirstParagraph =
+      article.fields.articleBody.content[0].content[0].value;
+    const description =
+      articleFirstParagraph.length > 100
+        ? articleFirstParagraph.substring(0, 100) + "..."
+        : articleFirstParagraph;
+    const fileUrl = article.fields.thumbnail
+      ? article.fields.thumbnail.fields.file.url
+      : undefined;
+    const categoryName = article.fields.category.fields.categoryName;
+    const tagNames = article.fields.tags.map((tag: any) => tag.fields.tagName);
+    const keywords =
+      "Kenji Wilkins,Blog,Engineer," + categoryName + "," + tagNames.join(",");
+    fs.readFile(__dirname + "/dist/index.html", "utf8", (err, htmlData) => {
+      if (err) {
+        console.error("Error during file reading", err);
+        return res.status(404).end();
+      }
+
+      // inject meta tags
+      htmlData = htmlData.replace(
+        '<meta property="og:title" content="KW Blog" />',
+        `<meta property="og:title" content="Kenji Wilkins - ${article.fields.title}" />`
+      );
+      htmlData = htmlData.replace(
+        '<meta property="og:type" content="website" />',
+        `<meta property="og:type" content="article" />`
+      );
+      htmlData = htmlData.replace(
+        '<meta name="keywords" content="Kenji Wilkins,Blog,Engineer," />',
+        `<meta name="keywords" content="${keywords}" />`
+      );
+      if (fileUrl) {
+        htmlData = htmlData.replace(
+          '<meta property="og:image" content="/logo.png" />',
+          `<meta property="og:image" content="${HTTPS}${fileUrl}" />`
+        );
+        htmlData = htmlData.replace(
+          '<meta name="twitter:image" content="/logo.png" />',
+          `<meta name="twitter:image" content="${HTTPS}${fileUrl}" />`
+        );
+      }
+      if (description) {
+        htmlData = htmlData.replace(
+          '<meta property="og:description" content="A blog by Kenji Wilkins" />',
+          `<meta property="og:description" content="${description}" />`
+        );
+        htmlData = htmlData.replace(
+          '<meta name="twitter:description" content="A blog by Kenji Wilkins" />',
+          `<meta name="twitter:description" content="${description}" />`
+        );
+      }
+      console.log(articleFirstParagraph);
+      res.send(htmlData);
+    });
+  } catch (error) {
+    if (error) {
+      console.error(error);
       return res.status(404).end();
     }
-
-    // inject meta tags
-    htmlData = htmlData.replace(
-      '<meta property="og:title" content="KW Blog" />',
-      `<meta property="og:title" content="Kenji Wilkins - ${article.fields.title}" />`
-    );
-    htmlData = htmlData.replace(
-      '<meta property="og:type" content="website" />',
-      `<meta property="og:type" content="article" />`
-    );
-    htmlData = htmlData.replace(
-      '<meta property="og:image" content="/logo.png" />',
-      `<meta property="og:image" content="${HTTPS}${fileUrl}" />`
-    );
-    res.send(htmlData);
-  });
+  }
 });
 
 // return index.html to all routes
